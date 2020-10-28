9d0d09e7f60938a60a5bd06a319f3e8857f31024
Sverre Spetalen <sverress@stud.ntnu.no>
2020-10-28 18:06:23 +0100

    Switched to using sets

--- TOP.py
+++ TOP.py
@@ -1,122 +1,137 @@
 import gurobipy as gp
 from gurobipy import GRB
-import numpy as np
 import helpers
+from itertools import product
 
 # Create a new model
-model = gp.Model("TOP")
+m = gp.Model("TOP")
+
+locations_coordinates = [
+    (0, 0),
+    (1, 5),
+    (3, 3),
+    (2, 4),
+    (3, 1),
+    (1, 3),
+]  # First element is depot
 
 # Constants
-S = 5  # Number of scooters (scooter 1 and 5 are depot)
-V = 1  # Number of service vehicles
+num_scooters = 3  # Number of scooters
+num_service_vehicles = 1  # Number of service vehicles
+num_locations = len(locations_coordinates)
 
-# Using random values for R and T
-np.random.seed(42)
-R = np.random.randint(100, size=(1, S))  # Reward for swapping battery for scooter i
-T = np.random.randint(
-    10, size=(S + 1, S + 1)
-)  # Time needed to travel from scooter i to j
-T_max = 10  # Duration of shift
-Q_b = 1  # Battery capacity of service vehicle v
+# Sets
+locations = range(num_locations)
+scooters = locations[1 : num_scooters + 1]
+delivery = locations[num_scooters + 1 :]
+service_vehicles = range(num_service_vehicles)
+depot = 0
 
-"""
-Create variables
+# Cartesian Products
+cart_locs = list(product(locations, repeat=2))
+cart_loc_v = list(product(locations, service_vehicles))
+cart_loc_loc_v = list(product(locations, locations, service_vehicles))
 
-x_ijv - 1 if, for service vehicle v, visit to scooter i is followed by a visit to scooter j- 0 otherwise
-b_iv - 1 if scooter i is visited by service vehicle v- 0 otherwise
-u_iv - position of scooter i for service vehicle v route
-"""
-x, b, u = [{} for i in range(3)]
-for i in range(1, S + 1):
-    for v in range(1, V + 1):
-        b[i, v] = model.addVar(vtype=GRB.BINARY, name=f"b_{i}_{v}")
-        u[i, v] = model.addVar(vtype=GRB.INTEGER, name=f"u_{i}_{v}")
-        for j in range(1, S + 1):
-            x[i, j, v] = model.addVar(vtype=GRB.BINARY, name=f"x_({i},{j})_{v}")
+# Parameters
+reward = (
+    [0.0]
+    + [(i + 1) / num_scooters - 0.1 for i in range(num_scooters)]
+    + [1.0] * (num_locations - num_scooters - 1)
+)  # Reward for visiting location i (uniform distribution). Eks for 3 locations [0, 0.33, 0.66]
+time_cost = {
+    (i, j): helpers.compute_distance(locations_coordinates[i], locations_coordinates[j])
+    for i, j in cart_locs
+}  # Calculate distance in time between all locations
+T_max = 15  # Duration of shift
+Q_b = [2] * num_service_vehicles  # Battery capacity of service vehicle v
 
-# Set objective
-model.setObjective(
-    gp.quicksum(R[0][i] * b[i, v] for i in range(2, S) for v in range(1, V + 1)),
-    GRB.MAXIMIZE,
+# Variables
+
+# x_ijv - 1 if, for service vehicle v, visit to location i is followed by a visit to location j- 0 otherwise
+x = m.addVars(cart_loc_loc_v, vtype=GRB.BINARY, name="x")
+# b_iv - 1 if location i is visited by service vehicle v- 0 otherwise
+b = m.addVars(cart_loc_v, vtype=GRB.BINARY, name="b")
+# u_iv - position of location i for service vehicle v route
+u = m.addVars(cart_loc_v, vtype=GRB.INTEGER, name="u")
+
+# Objective function
+m.setObjective(
+    gp.quicksum(reward[i] * b[(i, v)] for i, v in cart_loc_v), GRB.MAXIMIZE,
 )
+
+# Constraints
 
 # Add constraints (2): guarantee that each service vehicle starts and ends in at the depot.
-model.addConstr(
-    gp.quicksum(x[1, j, v] for v in range(1, V + 1) for j in range(2, S + 1)),
-    GRB.EQUAL,
-    V,
+m.addConstr(
+    gp.quicksum(x[(depot, j, v)] for j, v in cart_loc_v if j != depot)
+    == num_service_vehicles,
     "must_visit_depot_first",
 )
-model.addConstr(
-    gp.quicksum(x[i, S, v] for v in range(1, V + 1) for i in range(1, S)),
-    GRB.EQUAL,
-    V,
+m.addConstr(
+    gp.quicksum(x[(i, depot, v)] for i, v in cart_loc_v if i != depot)
+    == num_service_vehicles,
     "must_visit_depot_end",
 )
 
-# Add constraints (3): ensure that every scooter is visited at most once.
-for k in range(2, S):
-    model.addConstr(
-        gp.quicksum(b[k, v] for v in range(1, V + 1)),
-        GRB.LESS_EQUAL,
-        1,
-        f"only_one_visit_pr_scooter_(k={k})",
-    )
+# Add constraints (3): ensure that every location is visited at most once.
+m.addConstrs(
+    (
+        gp.quicksum(b[(k, v)] for v in service_vehicles) <= 1
+        for k in locations
+        if k != depot
+    ),
+    "only_one_visit_pr_scooter",
+)
 
-for v in range(1, V + 1):
-    model.addConstr(
-        gp.quicksum(b[k, v] for k in range(2, S)),
-        GRB.LESS_EQUAL,
-        Q_b,
-        f"battery_capacity_(k={k})",
-    )
+
+# Add constraints (4): ensure that each vehicle capacity is not exceeded
+m.addConstrs(
+    (gp.quicksum(b[(k, v)] for k in scooters) <= Q_b[v] for v in service_vehicles),
+    "battery_capacity",
+)
 
 # Add constraints (5): guarantee the connectivity of each service vehicle path
-for k in range(2, S):
-    for v in range(1, V + 1):
-        model.addConstr(
-            gp.quicksum(x[i, k, v] if i != k else 0 for i in range(1, S)),
-            GRB.EQUAL,
-            b[k, v],
-            f"connectivity_1_(k={k},v={v})",
-        )
-        model.addConstr(
-            gp.quicksum(x[k, j, v] if j != k else 0 for j in range(2, S + 1)),
-            GRB.EQUAL,
-            b[k, v],
-            f"connectivity_2_(k={k},v={v})",
-        )
+m.addConstrs(
+    (gp.quicksum(x[(i, k, v)] for i in locations) == b[(k, v)] for k, v in cart_loc_v),
+    "connectivity_inn",
+)
+
+m.addConstrs(
+    (gp.quicksum(x[(k, j, v)] for j in locations) == b[(k, v)] for k, v in cart_loc_v),
+    "connectivity_out",
+)
 
 # Add constraints (6): ensure that the length of the paths does not exceed the shift
-for v in range(1, V + 1):
-    model.addConstr(
-        gp.quicksum(T[i, j] * x[i, j, v] for i in range(1, S) for j in range(2, S + 1)),
-        GRB.LESS_EQUAL,
-        T_max,
-        f"time_constraints_(v={v})",
-    )
+m.addConstrs(
+    (
+        gp.quicksum(time_cost[(i, j)] * x[(i, j, v)] for i, j in cart_locs) <= T_max
+        for v in service_vehicles
+    ),
+    "time_constraints",
+)
+m.addConstrs((2 <= u[(i, v)] for i, v in cart_loc_v if i != depot), "subtours_1")
+m.addConstrs(
+    (u[(i, v)] <= num_locations for i, v in cart_loc_v if i != depot), "subtours_2"
+)
+m.addConstrs(
+    (
+        u[i, v] - u[j, v] + 1 <= (num_locations - 1) * (1 - x[i, j, v])
+        for i, j, v in cart_loc_loc_v
+        if i != j and i != depot
+    ),
+    "subtours_3",
+)
 
-# Add constraints (7-8): prevent subtours
-for i in range(2, S + 1):
-    for v in range(1, V + 1):
-        model.addConstr(2, GRB.LESS_EQUAL, u[i, v], f"subtours_1_(i={i},v={v})")
-        model.addConstr(u[i, v], GRB.LESS_EQUAL, S, f"subtours_2_(i={i},v={v})")
-        for j in range(2, S + 1):
-            if i != j:
-                model.addConstr(
-                    u[i, v] - u[j, v] + 1,
-                    GRB.LESS_EQUAL,
-                    (S - 1) * (1 - x[i, j, v]),
-                    f"subtours_3_(i,j=({i},{j}),v={v})",
-                )
 
 # Optimize model
-model.optimize()
+m.optimize()
 
 # Print solution
-for v in model.getVars():
+for v in m.getVars():
     if v.x > 0:
         print(f"{v.varName}: {int(v.x)}")
-print(f"Obj: {model.objVal}")
+print(f"Obj: {m.objVal}")
 
-helpers.print_model(model)
+print(f"Obj: {m.objVal}")
+
+helpers.print_model(m)
--- helpers.py
+++ helpers.py
@@ -1,9 +1,18 @@
-# Print model
+from math import sqrt
+
+
 def print_model(model, delete_file=True):
     import os
+
     model.write("model.lp")
     with open("model.lp") as f:
         for line in f.readlines():
             print(line)
     if delete_file:
         os.remove("model.lp")
+
+
+def compute_distance(loc1, loc2):
+    dx = loc1[0] - loc2[0]
+    dy = loc1[1] - loc2[1]
+    return sqrt(dx * dx + dy * dy)
