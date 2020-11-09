7f635769b7c322396801474453e3109d62dd9153
Sverre Spetalen <sverress@stud.ntnu.no>
2020-11-09 19:37:26 +0100

    added documentation of TestInstanceManager

--- TestInstanceManager.py
+++ TestInstanceManager.py
@@ -7,6 +7,11 @@ import math
 
 class TestInstanceManager:
     def __init__(self):
+        """
+        Class for creating multiple test instances from a given EnTur Dataset snapshot.
+        The class contains methods for fetching data, cleaning it and creating Instance instances to later be runned.
+        This class also contains methods for visualizing the incoming data.
+        """
         self._data_file_path = "test_data/bigquery-results.csv"
         self._data = self.get_data()
         self._bound = (
@@ -22,16 +27,19 @@ class TestInstanceManager:
             {}
         )  # Instances indexed by (num_of_sections, num_of_scooters_per_section)
 
-    def create_multiple_instances(self, instances_parameters: list):
-        for parameters in instances_parameters:
-            num_of_sections, num_of_scooters_per_section = parameters
-            self.instances[parameters] = self.create_test_instance(
-                num_of_sections, num_of_scooters_per_section
-            )
-
     def create_test_instance(
         self, num_of_sections: int, num_of_scooters_per_section: int,
     ):
+        """
+        Creates necessary data structures to create a new ModelInput object.
+
+        TODO: This methods input parameters are only related to zones and number of nodes.
+        TODO: Other parameters should be added optionally
+        :param num_of_sections: number of sections at each x and y axis. ex. 3 gives 9 zones
+        :param num_of_scooters_per_section: this is the number of scooters per zone and is also considered the optimal
+        state at the time of writing
+        :return: Input arguments to ModelInput class
+        """
         num_of_scooters = num_of_scooters_per_section * num_of_sections ** 2
         filtered_scooters = self.filter_data_lat_lon(self._data, self._bound)
         scooters = filtered_scooters.sample(
@@ -53,26 +61,59 @@ class TestInstanceManager:
             service_vehicles["bike"] = (1, 0, 5)
         return scooters, delivery_nodes, depot, service_vehicles
 
+    def create_multiple_instances(self, instances_parameters: list):
+        """
+        Generates multiple instances and stores them to the instances dict
+        :param instances_parameters: list of instance parameters.
+        ex: [(2,3)] -> add one instance with 2 sections and 3 scooters pr section
+        """
+        for parameters in instances_parameters:
+            num_of_sections, num_of_scooters_per_section = parameters
+            self.instances[parameters] = self.create_test_instance(
+                num_of_sections, num_of_scooters_per_section
+            )
+
     def set_random_state(self, new_state: int):
+        """
+        Function to change the random state (pandas thing) of the object. The random state is also used as the seed
+        :param new_state: integer number of the new random state
+        """
         self._random_state = new_state
         random.seed(self._random_state)
 
     def get_data(self, separator=";"):
+        """
+        Fetches data from csv file and adds datetime type to timestamp column.
+        This would be the place for additional data cleaning
+        :param separator:
+        :return:
+        """
         df = pd.read_csv(self._data_file_path, sep=separator)
         df["timestamp"] = pd.to_datetime(df["timestamp"])
         return df
 
     @staticmethod
-    def filter_data_lat_lon(df: pd.DataFrame, coordinates: tuple):
+    def filter_data_lat_lon(geospatial_data: pd.DataFrame, coordinates: tuple):
+        """
+        :param geospatial_data: dataframe with lat lon columns
+        :param coordinates: tuple with a size of four to define area to include locations from
+        :return: new dataframe containing locations within the given coordinates
+        """
         lat_min, lat_max, lon_min, lon_max = coordinates
-        return df[
-            (lon_min <= df["lon"])
-            & (df["lon"] <= lon_max)
-            & (lat_min <= df["lat"])
-            & (df["lat"] <= lat_max)
+        return geospatial_data[
+            (lon_min <= geospatial_data["lon"])
+            & (geospatial_data["lon"] <= lon_max)
+            & (lat_min <= geospatial_data["lat"])
+            & (geospatial_data["lat"] <= lat_max)
         ]
 
     def create_sections(self, number_of_sections: int):
+        """
+        Generate lat lon coordinates for the boundaries of sections
+        :param number_of_sections: integer
+        :return: tuple(dataframe with lat lon as columns containing the boundaries of the sections,
+        list of coordinates defining all zones generated)
+        """
         lat_min, lat_max, lon_min, lon_max = self._bound
         lon_length = lon_max - lon_min
         lat_length = lat_max - lat_min
@@ -97,9 +138,17 @@ class TestInstanceManager:
         return section_limits, section_coordinates
 
     @staticmethod
-    def create_delivery_nodes(df, coordinates, optimal_number):
+    def create_delivery_nodes(scooters, coordinates, optimal_number):
+        """
+        Generates delivery nodes for a zone. The number of generated delivery nodes is the difference between the
+        optimal state and the number of scooters in the area. The nodes are now generated randomly within the zone.
+        :param scooters: Dataframe containing the locations of all scooters
+        :param coordinates: tuple defining the bounds of the zone
+        :param optimal_number: the optimal number of scooters in this zone
+        :return: list of tuples with lat lon of the generated delivery locations
+        """
         lat_min, lat_max, lon_min, lon_max = coordinates
-        filtered_df = TestInstanceManager.filter_data_lat_lon(df, coordinates)
+        filtered_df = TestInstanceManager.filter_data_lat_lon(scooters, coordinates)
         return [
             (random.uniform(lat_min, lat_max), random.uniform(lon_min, lon_max))
             for i in range(optimal_number - len(filtered_df))
@@ -108,6 +157,12 @@ class TestInstanceManager:
     def visualize_test_data(
         self, scooters: pd.DataFrame, delivery_nodes: pd.DataFrame, num_of_sections: int
     ):
+        """
+        Generates a visual representation of the scooters and delivery nodes with a map in the background
+        :param scooters: dataframe for location of scooters
+        :param delivery_nodes: dataframe for location of delivery nodes
+        :param num_of_sections: number of section in each dimension
+        """
         lat_min, lat_max, lon_min, lon_max = self._bound
         fig, ax = plt.subplots()
 
@@ -141,7 +196,12 @@ class TestInstanceManager:
 
         plt.show()
 
-    def visualize_instance(self, instance_id):
+    def visualize_instance(self, instance_id: tuple):
+        """
+        Fetch the instance from the instance id given and use the visualize_test_data function to show this instance
+        :param instance_id: tuple(number of sections, number of scooters pr section)
+        :return: 
+        """
         num_of_sections, num_of_scooters_pr_section = instance_id
         scooters, delivery_nodes, depot, service_vehicles = self.instances[instance_id]
         self.visualize_test_data(scooters, delivery_nodes, num_of_sections)
