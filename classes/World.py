e93e9f58af460110b45f8c94aa3fdf72668d4b8f
Sverre Spetalen <sverress@stud.ntnu.no>
2021-03-02 16:28:01 +0100

    world class setup

---
+++ classes/World.py
@@ -0,0 +1,15 @@
+import clustering.scripts as clustering_scripts
+
+
+class World:
+    def __init__(self, shift_duration: int, sample_size=None, number_of_clusters=20):
+        self.shift_duration = shift_duration
+        self.state = clustering_scripts.get_initial_state(
+            sample_size=sample_size, number_of_clusters=number_of_clusters
+        )
+        self.stack = []
+        self.time = 0
+
+    def run(self):
+        while self.time < self.shift_duration:
+            self.stack.pop().perform()
