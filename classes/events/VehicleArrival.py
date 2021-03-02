cabc93f96ca6a5aa32f0914ae31e298af1b25eed
tordhaflan <tord.haflan@gmail.com>
2021-03-02 17:49:29 +0100

    merge master + bugfix in VehicleArrival

--- classes/events/VehicleArrival.py
+++ classes/events/VehicleArrival.py
@@ -2,13 +2,19 @@ from classes.events import Event
 
 
 class VehicleArrival(Event):
-    def __init__(self, arrival_time: int):
+    def __init__(self, arrival_time: int, arrival_cluster_id: int):
         super().__init__(arrival_time)
+        self.arrival_cluster_id = arrival_cluster_id
 
     def perform(self, world) -> None:
         """
         :param world: world object
         """
+        # get the cluster object that the vehicle has arrived to
+        arrival_cluster = world.state.get_cluster_by_id(self.arrival_cluster_id)
+
+        # set the arrival cluster as current cluster in state
+        world.state.current_cluster = arrival_cluster
 
         # TODO when implemented in decision: decision.get_best_action(state) -> action
         action = None
