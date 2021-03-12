86086f45595fe3e9cc1a555016270f313b535ef2
Sverre Spetalen <sverress@stud.ntnu.no>
2021-03-12 12:57:43 +0100

    test leave probabilities

--- classes/tests/test_state.py
+++ classes/tests/test_state.py
@@ -5,10 +5,26 @@ from clustering.scripts import get_initial_state
 
 class StateTests(unittest.TestCase):
     def setUp(self) -> None:
-        self.initial_state = get_initial_state(500)
+        self.state_mid = get_initial_state(500)
+        self.state_small = get_initial_state(100)
+        self.state_big = get_initial_state(2000)
 
     def test_visualize_clusters(self):
-        self.initial_state.visualize_clustering()
+        self.state_mid.visualize_clustering()
+
+    def test_leave_probabilities(self):
+        for state in [self.state_mid, self.state_small, self.state_mid]:
+            for cluster in state.clusters:
+                self.assertAlmostEqual(sum(cluster.get_leave_distribution()), 1)
+                self.assertFalse(
+                    any([prob < 0 for prob in cluster.get_leave_distribution()]),
+                    "There are negative probabilities in the move probabilities matrix",
+                )
+                self.assertFalse(
+                    any([prob > 1 for prob in cluster.get_leave_distribution()]),
+                    "There are probabilities bigger than one in the move probabilities matrix",
+                )
+                self.assertEqual(0, cluster.get_leave_distribution()[cluster.id])
 
 
 if __name__ == "__main__":
