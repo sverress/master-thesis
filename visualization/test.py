558e1b7dec1471fe00b3d755f3d87bc6268d8c60
tordhaflan <tord.haflan@gmail.com>
2021-03-18 15:18:07 +0100

    created test for running/analysis method

--- visualization/test.py
+++ visualization/test.py
@@ -4,8 +4,7 @@ A file for testing stuff without dealing with circular imports
 from clustering.scripts import get_initial_state
 from system_simulation.scripts import system_simulate
 from visualization.visualizer import *
-from classes.Cluster import Cluster
-from classes.Scooter import Scooter
+from analysis.evaluate_policies import run_analysis
 import unittest
 
 
@@ -34,6 +33,17 @@ class BasicVisualizerTests(unittest.TestCase):
         current_state.visualize_system_simulation(scooter_trips)
 
         current_state.visualize_flow(flows, next_state.current_cluster.id)
+
+    @staticmethod
+    def test_analysis():
+        run_analysis(
+            shift_duration=300,
+            sample_size=100,
+            number_of_clusters=10,
+            policies=["SwapAllPolicy"],
+            visualize_world=False,
+            smooth_curve=True,
+        )
 
 
 if __name__ == "__main__":
