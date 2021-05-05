26cbe2efe848ac2396e18c316957314d380006a1
Sverre Spetalen <sverress@stud.ntnu.no>
2021-05-05 20:11:34 +0200

    run setup

--- analysis/multiprocessing_training.py
+++ analysis/multiprocessing_training.py
@@ -27,10 +27,33 @@ def run_train_with_learning_rate(input_arg):
     train_linear_value_function(input_arg, f"lr_{input_arg}")
 
 
+def train_ann_network_structure(ann_structure, suffix):
+    world = classes.World(
+        480,
+        None,
+        clustering.scripts.get_initial_state(2500, 30),
+        verbose=False,
+        visualize=False,
+        ANN_NETWORK_STRUCTURE=ann_structure,
+        TRAINING_SHIFTS_BEFORE_SAVE=50,
+    )
+    world.policy = world.set_policy(
+        policy_class=decision.EpsilonGreedyValueFunctionPolicy,
+        value_function_class=decision.value_functions.ANNValueFunction,
+    )
+    train_value_function(world, save_suffix=f"{suffix}")
+
+
+def run_train_ann_network_structure(input_arg):
+    train_ann_network_structure(input_arg, f"struct_{input_arg}")
+
+
 def multiprocess_train(inputs, function):
     with Pool() as p:
         p.map(function, inputs)
 
 
 if __name__ == "__main__":
-    multiprocess_train([0.00001, 0.0001, 0.001, 0.01], run_train_with_learning_rate)
+    multiprocess_train(
+        [[100], [100] * 3, [100] * 5, [100] * 10], run_train_ann_network_structure
+    )
