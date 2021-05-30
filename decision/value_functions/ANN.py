188d988750431538ce1d37735551c6fbcbd2c9b2
Sverre Spetalen <sverress@stud.ntnu.no>
2021-05-30 21:09:00 +0200

    new activation function

--- decision/value_functions/ANN.py
+++ decision/value_functions/ANN.py
@@ -26,7 +26,7 @@ class ANN:
         self.update_predict_model()
 
         self.tensorboard = ModifiedTensorBoard(
-            log_dir=f"logs/relu_{learning_rate}_{network_structure}_{int(time.time())}",
+            log_dir=f"logs/sigmoid_{learning_rate}_{network_structure}_{int(time.time())}",
             profile_batch=100000000,  # https://github.com/tensorflow/tensorboard/issues/2819
         )
 
@@ -45,7 +45,7 @@ class ANN:
             model.add(
                 keras.layers.Dense(layer, kernel_regularizer=keras.regularizers.L2())
             )
-            model.add(keras.layers.Activation("relu"))
+            model.add(keras.layers.Activation("sigmoid"))
         # The last layer needs to have a single value function output
         model.add(keras.layers.Dense(1))
         optimizer = keras.optimizers.Adam(lr=self.learning_rate)
